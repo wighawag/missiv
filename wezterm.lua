954a09c1a6082c077195e45c47b37a8fa7017448
wighawag <wighawag@gmail.com>
2024-02-13 12:38:45 +0000

    init

---
+++ wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window
+  tab, pane, window = mux.spawn_window {
+    args = {'bash', '-i', '-c' , 'cd '.. cmd.args[1] .. '; bash'},
+
+
+  }
+  tab:set_title 'missiv'
+  window:set_title 'missiv'
+
+	local pane_server = pane:split {
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm server:dev; bash'},
+    direction = 'Bottom'
+  }
+	local pane_client = pane_server:split {
+    args = {'bash', '-i', '-c', 'cd '.. cmd.args[1] .. '; sleep 1; pnpm client:dev; bash'},
+    direction = 'Right'
+  }
+
+
+end)
+
+
+config = {}
+
+-- fix windows in virtualbox
+config.prefer_egl=true
+
+return config
