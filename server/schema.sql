eddbe508b905cb1ff2b5f896443142ad574928ac
wighawag <wighawag@gmail.com>
2024-02-26 11:08:08 +0000

    schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,18 +1,38 @@
-
-
 DROP TABLE IF EXISTS ComversationViews;
 CREATE TABLE IF NOT EXISTS ComversationViews (
     first         text        NOT NULL,
     second        text        NOT NULL,
     conversation  text        NOT NULL,
     lastMessage   timestamp   NOT NULL, 
-    accepted      boolean        NOT NULL,
+    accepted      boolean     NOT NULL,
     unread        boolean     NOT NULL,
-    PRIMARY KEY (first, second)
+    PRIMARY KEY (first, second),
+    FOREIGN KEY (first) REFERENCES Users (address),
+    FOREIGN KEY (second) REFERENCES Users (address)
 );
-CREATE INDEX idx_accepted ON ComversationViews (accepted);
-CREATE INDEX idx_unread ON ComversationViews (unread);
+CREATE INDEX IF NOT EXISTS idx_unread ON ComversationViews (first, accepted, unread);
+CREATE INDEX IF NOT EXISTS idx_accepted ON ComversationViews (first, accepted);
 
---DROP TABLE IF EXISTS Comversation;
---CREATE TABLE IF NOT EXISTS Customers (CustomerId INTEGER PRIMARY KEY, CompanyName TEXT, ContactName TEXT);
---INSERT INTO Customers (CustomerID, CompanyName, ContactName) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders'), (4, 'Around the Horn', 'Thomas Hardy'), (11, 'Bs Beverages', 'Victoria Ashworth'), (13, 'Bs Beverages', 'Random Name');
+
+DROP TABLE IF EXISTS Messages;
+CREATE TABLE IF NOT EXISTS  Messages
+(
+  conversation text      NOT NULL,
+  sender         text      NOT NULL,
+  timestamp    timestamp NOT NULL,
+  message      text      NOT NULL,
+  signature    text      NOT NULL,
+  PRIMARY KEY (conversation, sender, timestamp),
+  FOREIGN KEY (sender) REFERENCES Users (address)
+);
+CREATE INDEX IF NOT EXISTS idx_messsages ON Messages (conversation);
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE IF NOT EXISTS Users
+(
+  address text      NOT NULL,
+  keys    text      NOT NULL,
+  created timestamp NULL    ,
+  PRIMARY KEY (address)
+);
+CREATE INDEX IF NOT EXISTS idx_messsages ON Users (address);
