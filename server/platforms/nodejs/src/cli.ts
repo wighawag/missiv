18d0a827964fc4daa93a536a207692ca7691401f
wighawag <wighawag@gmail.com>
2024-03-30 09:59:55 +0000

    add nodejs server

---
+++ server/platforms/nodejs/src/cli.ts
@@ -0,0 +1,12 @@
+import { serve } from '@hono/node-server'
+import {createServer} from 'missiv-server-app';
+
+const app = createServer();
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
