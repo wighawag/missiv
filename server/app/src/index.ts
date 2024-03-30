f62f1871716613db3c75ddf5c4d4b4a1efa09789
wighawag <wighawag@gmail.com>
2024-03-30 10:16:56 +0000

    add cf

--- server/app/src/index.ts
+++ server/app/src/index.ts
@@ -1,11 +1,25 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 
 export function createServer(): Hono {
   const app = new Hono()
 
   app.get('/', (c) => {
-    return c.text('Hello ddddd!')
+    return c.text('Hello dddddddd!')
   })
 
   return app;
 }
+
+// export function handleWebsocket(func: (context: Context) => Promise<Res)
+
+// (c) => {
+//   return {
+//     onMessage(event, ws) {
+//       console.log(`Message from client: ${event.data}`)
+//       ws.send('Hello from server!')
+//     },
+//     onClose: () => {
+//       console.log('Connection closed')
+//     },
+//   }
+// }
---
+++ server/platforms/cf-worker-base/src/index.ts
@@ -0,0 +1,5 @@
+import { createServer } from 'missiv-server-app'
+
+const app = createServer()
+
+export default app
